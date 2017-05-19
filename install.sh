1cc13c48747d7977fa517d5cd80de76bfb53a18f
DH4 <wayne@bitstorm.pw>
2017-05-18 19:03:18 -0500

    Added font directory checks & user verification

--- install.sh
+++ install.sh
@@ -1,15 +1,58 @@
-if [[ $(id -u) -ne 0 ]] ;
-  then echo "This script requires sudo privileges" ;
-  exit 1 ;
-else
-  echo "Creating fonts folder..." &&
-  mkdir /usr/share/fonts/wps-fonts &&
-  echo "Moving files to the fonts folder..." &&
-  cp ./*.ttf /usr/share/fonts/wps-fonts &&
-  cp ./*.TTF /usr/share/fonts/wps-fonts &&
-  echo "Fixing permissions..." &&
-  chmod 644 /usr/share/fonts/wps-fonts/* &&
-  echo "Rebuilding fonts cache..." &&
-  fc-cache -vfs &&
-  echo "Successfully installed fonts!"
+#!/bin/bash
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+if [[ $(id -u) -ne 0 ]] ; then
+  echo "This script requires sudo privileges"
+  exit 1
 fi
+
+HOME_FONT="$HOME/.fonts"
+MOST_DISTROS="/usr/share/fonts"
+RHL5="/usr/X11R6/lib/X11/fonts"
+RHL6="/usr/X11R6/lib/X11/fonts"
+
+if test -e $MOST_DISTROS ; then
+        FONT_PATH=$MOST_DISTROS
+elif test -e $RHL5 ; then
+        FONT_PATH=$RHL5
+elif test -e $RHL6 ; then
+        FONT_PATH=$RHL6
+else
+        FONT_PATH=$HOME_FONT
+fi
+
+FONT_PATH=$FONT_PATH"/wps-fonts"
+
+if [ -d "$FONT_PATH" ]; then
+  read -p "Font Directory already exists, continue? [y/N] " -n 1 -r 
+fi
+
+if [[$REPLY == "" ]]; then
+  exit 0
+elif [[ $REPLY =~ ^[Nn]$ ]]; then
+  exit 0
+fi
+
+echo "Fonts will be installed in: "$FONT_PATH
+read -p "Continue with installation? [Y/n] " -n 1 -r
+
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+  exit 0
+fi
+
+if [ ! -d "$FONT_PATH" ]; then
+  echo "Creating Font Directory..."
+  mkdir $FONT_PATH
+fi
+
+echo "Installing Fonts..."
+cp *.ttf $FONT_PATH
+cp *.TTF $FONT_PATH
+echo "Fixing Permissions..."
+chmod 644 $FONT_PATH/*
+echo "Rebuilding Font Cache..."
+fc-cache -vfs
+echo "Installation Finished."
+
+exit 0
